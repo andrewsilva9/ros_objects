9a627bd8a9f09e46409863f7054de788e18e2fba
andrewsilva9 <asilva9@gatech.edu>
2017-03-12 14:51:46 -0400

    Identify tablets and change return to [x,y,w,h,id]
    Added checks to guess if the object is a tablet based on size and
    color. Return format also changed to [x, y, width, height, object_id].

--- scripts/object_detector.py
+++ scripts/object_detector.py
@@ -14,24 +14,31 @@ def find_objects(input_image):
 
     # Threshold and only allow super white through (table)
     ret, th1 = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)
-
+    tablet_id = 3
+    block_id = 2
+    inward_scale = 5
+    tablet_size = 2000
     contours, hierarchy = cv2.findContours(th1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     bounding_boxes = [cv2.boundingRect(c) for c in contours]
     final_cons = []
     for con in bounding_boxes:
         area = con[2] * con[3]
-        # If box is too large
-        if area > 3000:
-            continue
-        # If box is too small
-        if area < 100:
-            continue
+        mostly_black = area * 350
         mostly_white = area * 240
-        # If box is too white
-        if np.sum(gray[con[1]:con[1] + con[3], con[0]:con[0] + con[2]]) > mostly_white:
+        # If area is large and mostly black
+        if area > tablet_size:
+            if np.sum(img[con[1] + inward_scale:con[1] + con[3] - inward_scale,
+                      con[0] + inward_scale:con[0] + con[2] - inward_scale, :]) < mostly_black:
+                appender = [con[0], con[1] + down_shift, con[2], con[3], tablet_id]
+                final_cons.append(appender)
+        # If box is too small, ignore it
+        elif area < 100:
             continue
-        # TODO: if this is a tablet, continue...
-        # TODO: get better at ignoring cozmo boxes?
-        appender = [con[0], con[1] + down_shift, con[2], con[3]]
-        final_cons.append(appender)
+        # If box is too white, ignore it (cozmo box)
+        elif np.sum(gray[con[1]:con[1] + con[3], con[0]:con[0] + con[2]]) > mostly_white:
+            continue
+        # Else it's a block
+        else:
+            appender = [con[0], con[1] + down_shift, con[2], con[3], block_id]
+            final_cons.append(appender)
     return final_cons
